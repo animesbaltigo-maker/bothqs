54a4d61e9e51fa3001d93302efcee93956f254ba
animesbaltigo-maker <animesbaltigo@gmail.com>
2026-05-07 12:31:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from telegram import Update
-from telegram import (
+from telegram.ext import (
     Application,
     CallbackQueryHandler,
     CommandHandler,
